f96947c07965b4e11c5818aad9564796a8a85e94
casarin <nsgbanks@ciencias.unam.mx>
2025-04-02 22:09:47 -0600

    feat: add logger to Report Controller

--- src/main/java/com/iim/service/report/api/controller/ReportController.java
+++ src/main/java/com/iim/service/report/api/controller/ReportController.java
@@ -1,11 +1,13 @@
 package com.iim.service.report.api.controller;
 
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.slf4j.Logger;
 
 import static com.iim.service.report.api.constant.Path.baseUrl;
 import static com.iim.service.report.api.constant.Path.createReportMapping;
@@ -14,8 +16,14 @@ import static com.iim.service.report.api.constant.Path.createReportMapping;
 @RequestMapping(baseUrl)
 public class ReportController {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(ReportController.class);
+
     @GetMapping(createReportMapping)
     public ResponseEntity<?> createReport(@PathVariable String id, @RequestParam String template, @RequestParam String format) {
+        LOGGER.info("{Start Report Generator Application}");
+        LOGGER.info("Params: {}, {}, {}", id, template, format);
+
+
         return null;
     }
 }
